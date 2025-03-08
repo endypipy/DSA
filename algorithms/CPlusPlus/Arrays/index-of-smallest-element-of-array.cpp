9108073534a1f7236dbde3233b89312ecd25c781
endypipy <f2023266412@umt.edu.pk>
2025-03-08 23:42:00 +0500

    Update index-of-smallest-element-of-array.cpp
    With use of vector and also simple ones is in comment

--- algorithms/CPlusPlus/Arrays/index-of-smallest-element-of-array.cpp
+++ algorithms/CPlusPlus/Arrays/index-of-smallest-element-of-array.cpp
@@ -6,30 +6,65 @@ here smallest element of array is 1 which is at index 2 of array
 output = 2 that is index of smallest element of array.
 Time complexity = O(n)
 */
+#include <iostream>
+#include <vector> // Include vector header
 
-#include<iostream>
 using namespace std;
-int main()
-{
-    int size,j;
-    cout<<"enter the size of array";
-    cin>>size;
-    int arr[size];
-    for(int i=0;i<size;i++)
-    {
-        cin>>arr[i];
+
+int main() {
+    int size, j;
+    cout << "Enter the size of array: ";
+    cin >> size;
+
+    // Input validation: Check for non-positive size
+    if (size <= 0) {
+        cerr << "Error: Array size must be positive." << endl;
+        return 1; // Exit with an error code
     }
+
+    // Use std::vector instead of a raw array
+    vector<int> arr(size);
+
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
     //considering the element at zero index to be smallest
-    int min=arr[0];
-    int smallest_index=0;
+    int min = arr[0];
+    int smallest_index = 0;
     //comparing min to all other elements
-    for(j=0;j<size;j++)
-    {
-        if(min>arr[j])
-        {
-            min=arr[j];
-            smallest_index=j;
+    for (j = 0; j < size; j++) {
+        if (min > arr[j]) {
+            min = arr[j];
+            smallest_index = j;
         }
     }
-    cout<<"index of smallest element of array is"<<smallest_index;
+    cout << "Index of smallest element of array is " << smallest_index;
 }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int size,j;
+//     cout<<"enter the size of array";
+//     cin>>size;
+//     int arr[size];
+//     for(int i=0;i<size;i++)
+//     {
+//         cin>>arr[i];
+//     }
+//     //considering the element at zero index to be smallest
+//     int min=arr[0];
+//     int smallest_index=0;
+//     //comparing min to all other elements
+//     for(j=0;j<size;j++)
+//     {
+//         if(min>arr[j])
+//         {
+//             min=arr[j];
+//             smallest_index=j;
+//         }
+//     }
+//     cout<<"index of smallest element of array is"<<smallest_index;
+// }
